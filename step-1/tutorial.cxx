9718b905e563b51006f00ce3df941555fc9b1e9d
lswjkllc <lswjkllc@163.com>
2024-10-11 10:42:23 +0800

    feature: add 'specify c++ standard' and 'add version and configure file'

--- step-1/tutorial.cxx
+++ step-1/tutorial.cxx
@@ -5,15 +5,23 @@
 #include <cmath>
 #include <iostream>
 
+#include "TutorialConfig.h"
+
 int main(int argc, char *argv[])
 {
+    // report version
+    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+              << Tutorial_VERSION_MINOR << std::endl;
+
     if (argc < 2)
     {
+        // print usage
         std::cout << "Usage: " << argv[0] << " number" << std::endl;
         return 1;
     }
 
-    const double inputValue = atof(argv[1]);
+    // const double inputValue = atof(argv[1]);
+    const double inputValue = std::stod(argv[1]); // Specifying C++ standards
 
     // calculate square root
     const double outputValue = sqrt(inputValue);
