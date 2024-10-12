1acf761b613d3af7a83a2f422e398e3f3f8e1bb9
lswjkllc <lswjkllc@163.com>
2024-10-12 16:52:56 +0800

    feature: add export configure

---
+++ step-11/tutorial.cxx
@@ -0,0 +1,33 @@
+/*
+    A simple program that computes the square root of a number
+*/
+
+#include <iostream>
+
+#include "TutorialConfig.h"
+
+#include "MathFunctions.h"
+
+int main(int argc, char *argv[])
+{
+    // report version
+    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+              << Tutorial_VERSION_MINOR << std::endl;
+
+    if (argc < 2)
+    {
+        // print usage
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
+
+    // const double inputValue = atof(argv[1]);
+    const double inputValue = std::stod(argv[1]); // Specifying C++ standards
+
+    // calculate square root
+    const double outputValue = mathfunctions::sqrt(inputValue);
+    std::cout << "The square root of " << inputValue;
+    std::cout << " is " << outputValue << std::endl;
+
+    return 0;
+}
